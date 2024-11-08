1776c6f8a732e80b397a5e35cbd91859935dc136
Temirlan <temirlan.kazhigerey@gmail.com>
2024-11-08 11:59:20 +0500

    feat: add CORS origins

--- gateway_service/src/main.py
+++ gateway_service/src/main.py
@@ -1,5 +1,6 @@
 from uuid import uuid4
 from fastapi import FastAPI, WebSocket
+from fastapi.middleware.cors import CORSMiddleware
 from src.core.store import lifespan
 from src.routers.chat import chat_router
 from src.routers.podcast import gateway_router
@@ -9,6 +10,13 @@ def create_app() -> FastAPI:
     app = FastAPI(lifespan=lifespan)
     app.include_router(chat_router)
     app.include_router(gateway_router)
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["https://atlantys.kz", "http://localhost:3000"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
     return app
 
 app = create_app()
