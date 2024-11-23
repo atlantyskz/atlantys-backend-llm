f6a2f132e4de3de73a978093783b3023750d9961
Temirlan <temirlan.kazhigerey@gmail.com>
2024-11-23 20:21:15 +0500

    fix: routes

--- gateway_service/src/main.py
+++ gateway_service/src/main.py
@@ -28,7 +28,7 @@ def custom_openapi():
     return app.openapi_schema
 
 def register_static_docs_routers(app: FastAPI):
-    @app.get("/docs", include_in_schema=False)
+    @app.get("/api/docs", include_in_schema=False)
     async def custom_swagger_ui_html():
         return get_swagger_ui_html(
                 openapi_url=app.openapi_url,
@@ -42,7 +42,7 @@ def register_static_docs_routers(app: FastAPI):
     async def swagger_ui_redirect():
         return get_swagger_ui_oauth2_redirect_html()
 
-    @app.get("/redoc", include_in_schema=False)
+    @app.get("/api/redoc", include_in_schema=False)
     async def redoc_html():
         return get_redoc_html(
             openapi_url=app.openapi_url,
@@ -50,7 +50,7 @@ def register_static_docs_routers(app: FastAPI):
             redoc_js_url="https://cdn.jsdelivr.net/npm/redoc@2.0.0-rc.55/bundles/redoc.standalone.js",
         )
 
-    @app.get("api/openapi.json", include_in_schema=False)
+    @app.get("/api/openapi.json", include_in_schema=False)
     async def get_openapi_json():
         schema = app.openapi()
         print(f"OpenAPI schema requested. Schema size: {len(str(schema))} characters")
