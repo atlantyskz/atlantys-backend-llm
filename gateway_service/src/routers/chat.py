ef462fe6459607570ed8dd9f9ebfc75cc3434584
Temirlan <temirlan.kazhigerey@gmail.com>
2024-11-27 11:09:36 +0500

    fix: websocket router

--- gateway_service/src/routers/chat.py
+++ gateway_service/src/routers/chat.py
@@ -1,10 +1,9 @@
-from fastapi import WebSocket, APIRouter, Depends
-from uuid import uuid4
 import datetime
-
+from uuid import uuid4
+from fastapi import WebSocket, APIRouter, Depends, WebSocketDisconnect
 from fastapi.websockets import WebSocketState
 from src.core.enums import LLM_ENDPOINT
-from src.models.chat import ChatDialogueHistory, Message
+from src.models.chat import ChatDialogueHistory
 from src.services.ai_chat import AiChatService, get_service as get_ai_chat_service
 
 chat_router = APIRouter(prefix='/api')
@@ -34,10 +33,10 @@ async def websocket_endpoint(
 
     await websocket.send_json({"session_id": session_id})
     print(f"Session ID: {session_id} connected.")
-    await websocket.send_text("Connection established")
+    if websocket.application_state == WebSocketState.CONNECTED:
+        await websocket.send_text("Connection established")
     bot_intro_message = "Здравствуйте! Я представляю компанию Atlantys. Мы помогаем бизнесам решать задачи и делать работу проще с помощью ИИ решений. Как я могу к вам обращаться?"
     await chat_history.add_message(sender="bot", message=bot_intro_message) 
-    print("Sent intro message to user.")
     try:
         while True:
             print("Waiting for data")
@@ -70,14 +69,17 @@ async def websocket_endpoint(
             await websocket.send_json(bot_response)
             print("Chat history updated successfully.")
 
+    except WebSocketDisconnect:
+        print("Client disconnected.")
     except Exception as e:
-        print(f"Exception occurred: {e}") 
+        print(f"Exception occurred: {e}")
         if websocket.client_state == WebSocketState.CONNECTED:
             await websocket.send_text(f"Error: {str(e)}")
     finally:
         if websocket.client_state == WebSocketState.CONNECTED:
             await websocket.send_text("Connection closed")
             await websocket.close()
+
 
 @chat_router.get("/chat-history/{session_id}/summary")
 async def get_chat_history(session_id: str, ai_chat_service: AiChatService = Depends(get_ai_chat_service)):
